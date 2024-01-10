2b72f465acbbfbdc65747653159e153fde656013
WooZhiSheng <1211205502@student.mmu.edu.my>
2024-01-10 23:17:41 +0800

    s

--- functions/database_functions.php
+++ functions/database_functions.php
@@ -1,125 +1,103 @@
 <?php
-	function db_connect(){
-		$conn = mysqli_connect("localhost", "root", "", "www_project");
-		if(!$conn){
-			echo "Can't connect database " . mysqli_connect_error($conn);
-			exit;
-		}
-		return $conn;
-	}
+include 'dataconnection.php';
 
-	function select4LatestBook($conn){
-		$row = array();
-		$query = "SELECT book_isbn, book_image FROM books ORDER BY book_isbn DESC";
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-		    echo "Can't retrieve data " . mysqli_error($conn);
-		    exit;
-		}
-		for($i = 0; $i < 4; $i++){
-			array_push($row, mysqli_fetch_assoc($result));
-		}
-		return $row;
-	}
+$conn = db_connect();
 
-	function getBookByIsbn($conn, $isbn){
-		$query = "SELECT book_title, book_author, book_price FROM books WHERE book_isbn = '$isbn'";
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-			echo "Can't retrieve data " . mysqli_error($conn);
-			exit;
-		}
-		return $result;
-	}
+function select4LatestBook($conn,$isbn){
+    $row = array();
+    $query = "SELECT book_isbn, book_image FROM books ORDER BY book_isbn DESC LIMIT 4";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        echo "Can't retrieve data " . mysqli_error($conn);
+        exit;
+    }
+    while($row[] = mysqli_fetch_assoc($result));
+    array_pop($row);
+    return $row;
+}
 
-	function getOrderId($conn, $customerid){
-		$query = "SELECT orderid FROM orders WHERE customerid = '$customerid'";
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-			echo "retrieve data failed!" . mysqli_error($conn);
-			exit;
-		}
-		$row = mysqli_fetch_assoc($result);
-		return $row['orderid'];
-	}
+function getBookByIsbn($conn, $isbn){
+    $query = "SELECT book_title, book_author, book_price FROM books WHERE book_isbn = '$isbn'";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        echo "Can't retrieve data " . mysqli_error($conn);
+        exit;
+    }
+    return $result;
+}
 
-	function insertIntoOrder($conn, $customerid, $total_price, $date, $ship_name, $ship_address, $ship_city, $ship_zip_code, $ship_country){
-		$query = "INSERT INTO orders VALUES 
-		('', '" . $customerid . "', '" . $total_price . "', '" . $date . "', '" . $ship_name . "', '" . $ship_address . "', '" . $ship_city . "', '" . $ship_zip_code . "', '" . $ship_country . "')";
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-			echo "Insert orders failed " . mysqli_error($conn);
-			exit;
-		}
-	}
+function getOrderId($conn, $customerid){
+    $query = "SELECT orderid FROM orders WHERE customerid = '$customerid'";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        echo "Retrieve data failed!" . mysqli_error($conn);
+        exit;
+    }
+    $row = mysqli_fetch_assoc($result);
+    return $row['orderid'];
+}
 
-	function getbookprice($isbn){
-		$conn = db_connect();
-		$query = "SELECT book_price FROM books WHERE book_isbn = '$isbn'";
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-			echo "get book price failed! " . mysqli_error($conn);
-			exit;
-		}
-		$row = mysqli_fetch_assoc($result);
-		return $row['book_price'];
-	}
+function insertIntoOrder($conn, $customerid, $total_price, $date, $ship_name, $ship_address, $ship_city, $ship_zip_code, $ship_country){
+    $query = "INSERT INTO orders VALUES 
+    ('', '" . $customerid . "', '" . $total_price . "', '" . $date . "', '" . $ship_name . "', '" . $ship_address . "', '" . $ship_city . "', '" . $ship_zip_code . "', '" . $ship_country . "')";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        echo "Insert orders failed " . mysqli_error($conn);
+        exit;
+    }
+}
 
-	function getCustomerId($name, $address, $city, $zip_code, $country){
-		$conn = db_connect();
-		$query = "SELECT customerid from customers WHERE 
-		name = '$name' AND 
-		address= '$address' AND 
-		city = '$city' AND 
-		zip_code = '$zip_code' AND 
-		country = '$country'";
-		$result = mysqli_query($conn, $query);
-		// if there is customer in db, take it out
-		if($result){
-			$row = mysqli_fetch_assoc($result);
-			return $row['customerid'];
-		} else {
-			return null;
-		}
-	}
+function getBookPrice($conn, $isbn){
+    $query = "SELECT book_price FROM books WHERE book_isbn = '$isbn'";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        echo "Get book price failed! " . mysqli_error($conn);
+        exit;
+    }
+    $row = mysqli_fetch_assoc($result);
+    return $row['book_price'];
+}
 
-	function setCustomerId($name, $address, $city, $zip_code, $country){
-		$conn = db_connect();
-		$query = "INSERT INTO customers VALUES 
-			('', '" . $name . "', '" . $address . "', '" . $city . "', '" . $zip_code . "', '" . $country . "')";
+function getCustomerId($conn, $name, $address, $city, $zip_code, $country){
+    $query = "SELECT customerid FROM customers WHERE 
+    username = '$name' AND 
+    address = '$address' AND 
+    city = '$city' AND 
+    zip_code = '$zip_code' AND 
+    country = '$country'";
+    $result = mysqli_query($conn, $query);
+    if($result && mysqli_num_rows($result) > 0){
+        $row = mysqli_fetch_assoc($result);
+        return $row['customerid'];
+    } else {
+        return null;
+    }
+}
 
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-			echo "insert false !" . mysqli_error($conn);
-			exit;
-		}
-		$customerid = mysqli_insert_id($conn);
-		return $customerid;
-	}
+function setCustomerId($conn, $name, $address, $city, $zip_code, $country){
+    $query = "INSERT INTO customers (username, address, city, zip_code, country) VALUES 
+        ('$name', '$address', '$city', '$zip_code', '$country')";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        echo "Insert customers failed! " . mysqli_error($conn);
+        exit;
+    }
+}
 
-	function getPubName($conn, $pubid){
-		$query = "SELECT publisher_name FROM publisher WHERE publisherid = '$pubid'";
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-			echo "Can't retrieve data " . mysqli_error($conn);
-			exit;
-		}
-		if(mysqli_num_rows($result) == 0){
-			echo "Empty books ! Something wrong! check again";
-			exit;
-		}
+function insertIntoOrderDetails($conn, $orderid, $isbn, $quantity, $price){
+    $query = "INSERT INTO order_details VALUES 
+    ('" . $orderid . "', '" . $isbn . "', '" . $quantity . "', '" . $price . "')";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        echo "Insert order details failed! " . mysqli_error($conn);
+        exit;
+    }
+}
 
-		$row = mysqli_fetch_assoc($result);
-		return $row['publisher_name'];
-	}
+$latestBooks = select4LatestBook($conn);
+foreach($latestBooks as $book){
+    echo "ISBN: " . $book['book_isbn'] . ", Image: " . $book['book_image'] . "<br>";
+}
 
-	function getAll($conn){
-		$query = "SELECT * from books ORDER BY book_isbn DESC";
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-			echo "Can't retrieve data " . mysqli_error($conn);
-			exit;
-		}
-		return $result;
-	}
+mysqli_close($conn);
 ?>
